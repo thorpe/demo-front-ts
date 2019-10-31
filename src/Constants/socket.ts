7d4820730e27a29e55875c4a9eafe82ecda8e03c
dev.lkb <dev.lkb@iventory.co.kr>
2019-10-31 14:08:47 +0900

    원 손스 분석하기중

--- src/Constants/socket.ts
+++ src/Constants/socket.ts
@@ -1,13 +1,13 @@
 export enum SOCKET_TYPE {
   SOCKETIO = 'socket.io',
-  WEBSOCKET = 'websocket'
+  WEBSOCKET = 'websocket',
 }
 
 export const SOCKER_TYPES: SOCKET_TYPE[] = [SOCKET_TYPE.SOCKETIO, SOCKET_TYPE.WEBSOCKET]
 
 export enum DATA_FORMAT_TYPE {
   JSON = 'json',
-  TEXT = 'text'
+  TEXT = 'text',
 }
 
 export const DATA_FORMATS: DATA_FORMAT_TYPE[] = [DATA_FORMAT_TYPE.JSON, DATA_FORMAT_TYPE.TEXT]
